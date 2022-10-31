79bdcdfc4e2a9c244c640b9aa1493b7e94b508dd
jghomel <jghomel@gmail.com>
2022-10-31 15:31:35 -0400

    Changed button colors to make them actually ok Wrapper around search box to start creating margins for diplaying blocks of content

--- src/components/ButtonWrapper.js
+++ src/components/ButtonWrapper.js
@@ -1,13 +1,18 @@
 import { Link, makeStyles, Button } from '@material-ui/core'
+import { spacing } from "@material-ui/system";
 
 import * as React from 'react'
 
 
 const useStyles = makeStyles({
   buttonWrapper: {
-    background:"red",
-    color:"black",
-    "&:hover":"black",
+    background:"#797d9e",
+    color:"white",
+    margin:10,
+    fontWeight:"bold",
+    '&:hover': {
+      backgroundColor: '#36454F',
+  },
   },
 
 });
@@ -15,7 +20,8 @@ const useStyles = makeStyles({
 
 export default function ButtonWrapper(props) {
       const classes = useStyles();
-  //props
+
+      //props
     const { children, handleClick } = props
   return (
     <Button variant="contained" className={classes.buttonWrapper} onClick={handleClick}>{children}</Button>
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,8 @@
-import { Button, Container, CssBaseline, TextField } from "@mui/material"
+import { ClassNames } from "@emotion/react";
+import { Box, Button, Container, CssBaseline, TextField } from "@mui/material"
 import React, {useState} from "react"
 import ButtonWrapper from "./ButtonWrapper"
+
 
 export default function Search(props) {
 
@@ -13,8 +15,12 @@ export default function Search(props) {
   const handleClick = props.handleClick
   return (
     <>
-      <TextField label={fieldLabel} onChange={(e) => setInput(e.target.value)} onKeyPress={(e) => {if (e.key === 'Enter') handleClick(input) }} />
-      <ButtonWrapper handleClick={() => handleClick(input)}>{buttonLabel}</ButtonWrapper>
+      <Box sx={{
+        m:3,
+        display: 'flex',
+      }}> <TextField label={fieldLabel} onChange={(e) => setInput(e.target.value)} onKeyPress={(e) => {if (e.key === 'Enter') handleClick(input) }} />
+      <ButtonWrapper handleClick={() => handleClick(input)}>{buttonLabel}</ButtonWrapper> </Box>
+
     </>
   )
 }
