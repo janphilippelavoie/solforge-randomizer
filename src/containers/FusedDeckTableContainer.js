cc9697f0c644119b4085401d390ce8702726b0e5
JP Theriault <drkangel666@hotmail.com>
2022-11-16 09:52:20 -0500

    Implemented draw 5. Added a column to fused deck page to temporarily display results.

--- src/containers/FusedDeckTableContainer.js
+++ src/containers/FusedDeckTableContainer.js
@@ -10,7 +10,8 @@ export default function FusedDeckTableContainer(props) {
     const headers = [
       { id: "name", label: "Name", number: false },
       { id: "deck1", label: "First Deck", number: false },
-      { id: "deck2", label: "Second Deck", number: false }
+      { id: "deck2", label: "Second Deck", number: false },
+      { id: "hand", label: "Random Hand", number: false}
     ]
   
     function getRows() {
@@ -21,7 +22,8 @@ export default function FusedDeckTableContainer(props) {
             "deck1": deck.decks[0].name,
             "deck1Id": deck.decks[0].id,
             "deck2": deck.decks[1].name,
-            "deck2Id": deck.decks[1].id
+            "deck2Id": deck.decks[1].id,
+            "hand": deck.getRandomHand().map((card) => card.title).sort().join(", ")
           }
         });
     }
